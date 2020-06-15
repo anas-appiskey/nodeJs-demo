a230bd2caf0e5082cd90620010fc547459324079
anasali2193 <anasali2193@gmail.com>
2020-06-15 21:13:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,74 @@
 var http  = require('http');
 var path  = require('path');
 var fs  = require('fs');
+const { Z_NEED_DICT } = require('zlib');
 
 const server =http.createServer((req,res)=>{
-    if(req.url==='/'){
-        res.writeHead(200,{'Content-Type': 'text/html'});
-        res.end('<h1>home page</h1>')
+    // if(req.url==='/'){
+    //     fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+    //         if (err) throw err;
+    //         res.writeHead(200,{'Content-Type': 'text/html'});
+    //         res.end(content)
+    //     })
+   
+    // }
+    // if(req.url==='/api/users'){
+    //     const users =[
+    //         {name : 'anas',age: 21, id:1},
+    //         {name : 'jasicca',age: 21, id:2},
+    //     ];
+    //     res.writeHead(200,{'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users))
+   
+    // }
+
+    //building file path 
+    let filepath = path.join(
+        __dirname,'public',
+        req.url ==='/' ?'index.html':
+         req.url);
+         
+    // get extension 
+    let extname = path.extname(filepath);
+
+    //initial content type 
+    let contentType = 'text/html';
+
+    //check ext and set content type
+
+    switch(extname){
+        case '.js':
+            contentType: 'text/javascript';
+            break;
+        case '.css':
+            contentType: 'text/javascript';
+            break;
+        case '.json':
+            contentType: 'application/json';
+            break; 
+        case '.png':
+            contentType: 'image/png';
+            break;
+            
+        case '.jpg':
+            contentType: 'image/jpg';
+            break;
+
+
     }
+    fs.readFile(filepath,(err,content)=>{
+        if(err){
+            if(err.code==='ENONET'){
+                //page not found
+                fs.readFile(path.join(__dirname,'public','404.html'),(err,content)=>{
+
+                    res.writeHead(200,{'Content-Type': 'text/html'});
+                    res.end(content,'utf8')
+
+                })
+            }
+        }
+    })
 })
 
 const PORT = process.env.PORT || 5000
